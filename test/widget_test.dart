9ad0ac022a1ec71ed735f391c90b2684931459a7
Sazidul islam <rabbiking00@gmail.com>
2024-10-16 09:36:30 +0600

    Rozarpay Updated

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,8 +6,8 @@ import 'package:razorpay_pyment/main2.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    // await tester.pumpWidget(MyApp());
     await tester.pumpWidget(MyApp());
+    // await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
